ee3be98bbe6123eccb833b247811c5c83090ff60
paleonluna <paleonluna@gmail.com>
2016-05-23 22:16:45 -0700

    first push

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public Animator thisAnimator;
+	const string walkingBool = "isWalking";
+	float walkAxis, rotateAxis;
+	float speedScalar = .04f, rotateScalar = 4f;
+
+	public float hunger, anxiety;
+	public int devLevel, money;
+
+	public static PlayerController s_instance;
+
+	void Awake() {
+		if (s_instance == null) {
+			s_instance = this;
+		} else {
+			Destroy (this);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		walkAxis = Input.GetAxis ("Vertical");
+		rotateAxis = Input.GetAxis ("Horizontal");
+		HandleMovement ();
+		HandleRotation ();
+		HandleAnimation ();
+	}
+
+	void HandleMovement() {
+		transform.Translate (Vector3.forward * speedScalar * walkAxis);
+	}
+
+	void HandleRotation () {
+		transform.Rotate (0, rotateAxis * rotateScalar,0);
+	}
+
+	void HandleAnimation () {
+		if (walkAxis > 0) {
+			thisAnimator.SetBool (walkingBool, true);
+		} else {
+			thisAnimator.SetBool (walkingBool, false);
+		}
+	}
+}
---
+++ Assets/Scripts/GUI/MetricFader.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class MetricFader : MonoBehaviour {
+
+	float fadeTime;
+	bool fadeIn;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
