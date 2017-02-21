15331896f0364352f3c42618d6f5d53b2e88d15a
Zack Rock <zacharyrock@hotmail.com>
2017-02-20 20:41:29 -0800

    Sandbox stuff.

---
+++ Assets/Sandbox/Zack/BaseInput.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseInput : MonoBehaviour {
+
+	public Vector3 dir;
+	public bool sprint;
+//	public bool jump;
+//	public bool grapple;
+	public bool aim;
+	public bool shoot;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+}
