7be00152b7af106dc663511cde4ffddf0b5ed40e
paleonluna <paleonluna@gmail.com>
2017-02-23 00:42:58 -0800

    added in SoundtrackManager.cs

---
+++ Assets/Scripts/Managers/SoundtrackManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundtrackManager : MonoBehaviour {
+
+	public AudioSource pink, water, drowning, chat, melancholy, ambient, jacob, wind, lakeAmbient, megaAmbient, nightAmbience, insignificance, donnie, three, garby; //soundtrack files
+	public static SoundtrackManager s_instance;
+
+	void Awake () {
+		s_instance = this; 
+		DontDestroyOnLoad (gameObject); //persist through scenes
+	}
+
+	IEnumerator FadeOutAudioSource(AudioSource x) { //call from elsewhere
+		while (x.volume > 0.0f) {					//where x is sound track file
+			x.volume -= 0.01f;
+			yield return new WaitForSeconds(0.03f);
+		}
+		x.Stop ();
+	}
+
+	public void PlayAudioSource(AudioSource x) { //call from elsewhere
+		x.volume = 1;
+		x.Play ();
+	}
+}
