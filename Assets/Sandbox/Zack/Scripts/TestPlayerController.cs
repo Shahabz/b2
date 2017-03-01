6b5f4c71c2e10bd76cbb42fe804436b31a1517f7
Zack Rock <zacharyrock@hotmail.com>
2017-02-28 22:25:51 -0800

    Removed trail. Added David to School.

--- Assets/Sandbox/Zack/Scripts/TestPlayerController.cs
+++ Assets/Sandbox/Zack/Scripts/TestPlayerController.cs
@@ -12,30 +12,10 @@ public class TestPlayerController : MonoBehaviour {
 	Animator anim;
 //	public Animator topAnimator;
 //	public Animator bottomAnimator;
-	
-//	public LayerMask mask;
-	
-//	public float jumpStrength = 3.0f;
-	
-//	float snowLevel = 0.0f;
-	
-//	public float snowAccumSpeed = 0.2f;
-	
-//	bool moved;
 
 	Rigidbody rigidbody;
 	
-//	[System.NonSerialized]					
-//	public float lookWeight;					// the amount to transition when using head look	
-//	
-//	public float lookSmoother = 3f;				// a smoothing setting for camera motion
-	
 	public Transform cameraObj;
-
-//	public Transform hand;
-
-//	public float attackRadius = 1.0f;
-
 	LineRenderer laserTarget;
 	
 	void Start () {
@@ -46,32 +26,6 @@ public class TestPlayerController : MonoBehaviour {
 	}
 	
 	void Update () {
-//		if(Input.GetKeyDown(KeyCode.T)) {
-//			Application.LoadLevel(0);
-//		}
-//		if(Input.GetKeyDown(KeyCode.Escape)) {
-//			Application.Quit();
-//		}
-//		//-------------------Snow stuff---------------------//
-//		if (moved) {
-//			snowLevel -= snowAccumSpeed * 5.0f * Time.deltaTime;
-//			if(snowLevel < 0.1f) {
-//				snowLevel = 0.1f;
-//				moved = false;
-//			}
-//		} else {
-//			snowLevel += snowAccumSpeed * Time.deltaTime;
-//			if (snowLevel > 1.0f)
-//				snowLevel = 1.0f;
-//			if (rigidbody.velocity.sqrMagnitude > 0.05f || input.dir.magnitude > 0.0f) 
-//				moved = true;
-//		}
-//		mat.SetFloat ("_Snow", snowLevel);
-//		
-//		bottomAnimator.SetFloat ("SnowLevel", snowLevel);
-//		topAnimator.SetFloat ("SnowLevel", snowLevel);
-		
-		//---------------------------------------------------//
 		//---------------------Movement----------------------//
 		float speedMod = input.sprint ? sprintSpeedMod : 1.0f;
 		rigidbody.angularVelocity = Vector3.zero;
@@ -91,12 +45,10 @@ public class TestPlayerController : MonoBehaviour {
 				transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation (lookDir), turnSpeed*Time.deltaTime);
 		}
 
-		//TODO fix this
+		//Wonky code because idle: 0, walk: 0.5, sprint: 1
 		anim.SetFloat("Sprint", input.sprint ? Mathf.Lerp(anim.GetFloat("Sprint"), 1f, 5f*Time.deltaTime) :  Mathf.Lerp(anim.GetFloat("Sprint"), 0f, 5f*Time.deltaTime));
 		anim.SetFloat("Movement", Mathf.Lerp(anim.GetFloat("Movement"), input.moveDir.normalized.magnitude/2f + anim.GetFloat("Sprint")/2f, 14f*Time.deltaTime));
 		anim.SetBool("Aim", input.aim);
-
-		transform.FindChild("Trail").gameObject.SetActive(input.sprint);
 
 		if(input.aim) {
 			Camera.main.GetComponent<CameraFollow>().distanceMax = Mathf.Lerp(Camera.main.GetComponent<CameraFollow>().distanceMax, 1f, Time.deltaTime*5f);
@@ -111,7 +63,7 @@ public class TestPlayerController : MonoBehaviour {
 			lookDir.y = 0.0f;
 			transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation (lookDir), 15f*Time.deltaTime);
 
-//			RaycastHit hit = new RaycastHit();
+//			RaycastHit hit = new RaycastHit(); 
 //			if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit)) {
 			laserTarget.gameObject.SetActive(true);
 			laserTarget.SetPositions( new Vector3[] {laserTarget.transform.position, laserTarget.transform.position + (laserTarget.transform.up * 50f)});
@@ -141,36 +93,10 @@ public class TestPlayerController : MonoBehaviour {
 			anim.SetTrigger("punch");
 			GetComponent<WeaponManager>().Melee();
 		}
-
-//		if(Input.GetButton("Fire2"))
-//		{
-//			// ...set a position to look at with the head, and use Lerp to smooth the look weight from animation to IK (see line 54)
-//			topAnimator.SetLookAtPosition(enemy.transform.position);
-//			lookWeight = Mathf.Lerp(lookWeight,1f,Time.deltaTime*lookSmoother);
-//			
-//		}
-//		// else, return to using animation for the head by lerping back to 0 for look at weight
-//		else
-//		{
-//			lookWeight = Mathf.Lerp(lookWeight,0f,Time.deltaTime*lookSmoother);
-//		}
-//		topAnimator.SetLookAtWeight(lookWeight);
 	}
 
 	public void AimDone() {
 		GetComponent<RootMotion.FinalIK.LookAtIK>().enabled = false;
 		GetComponent<RootMotion.FinalIK.AimIK>().enabled = true;
 	}
-	
-//	bool CheckGrounded() {
-//		Debug.DrawRay (transform.position + (Vector3.up * 0.1f), Vector3.down, Color.red, 1.0f);
-//		return Physics.Raycast (transform.position + Vector3.up, Vector3.down, GROUND_CHECK_DISTANCE, mask);
-//	}
-	
-//	void Attack() {
-//		Collider[] cols = Physics.OverlapSphere(hand.transform.position, attackRadius);
-//		foreach(Collider col in cols) {
-//			col.SendMessage("Die", SendMessageOptions.DontRequireReceiver);
-//		}
-//	}
 }
