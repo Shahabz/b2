1fc4b4c5effc1457f3159a7619bd26a60359c8ed
paleonluna <paleonluna@gmail.com>
2017-04-30 23:01:53 -0700

    update to factory level and rapsong scripts

---
+++ Assets/Scripts/RapSongObject.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RapSongObject : ScriptableObject {
+
+	public List<RapSongObjectElement> rapSongObjectElements;
+}
--- Assets/Scripts/RapTherapyManager.cs
+++ Assets/Scripts/RapTherapyManager.cs
@@ -6,13 +6,15 @@ public class RapTherapyManager : MonoBehaviour {
 
 
 	/*
-	 * 
-	 * 
-	 * 
-	 * 
-	/*
+	 * BPM/60 = BPS
+	 * We will have a canvas that has 4 UI Measure elements each spaced at a beat apart, or quarter note apart, 
+	 * Quarter note gets the beat so every second that passes, the floating Note objects will pass the distance between two UI Measure elements 
+	 * When the play inputs a value, if the distance between the next note and the Final measure object is a small enough value, you succeed
+	 * else the vocals cut out and a bark plays saying that you fucked up that section
+	*/
 
-
+	AudioSource currentRapBeat, currentVocalTrack;
+	AudioSource[] barks;
 
 
 	float currentSongBPM;
