d0f1668e2e41d0d4d03ade5c999ea2f7760301da
paleonluna <paleonluna@gmail.com>
2017-05-24 10:27:18 -0700

    addded in elevator test, player doesnt stick to the elevator though

--- Assets/Scripts/LevelObjects/Elevator.cs
+++ Assets/Scripts/LevelObjects/Elevator.cs
@@ -22,18 +22,29 @@ public class Elevator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
 		switch (thisElevatorState) {
 		case ElevatorState.Ascending:
-
+			transform.Translate (Vector3.Normalize (EndPoint.position - StartPoint.position) * travelSpeed);
+			if (Vector3.Distance (transform.position, EndPoint.position) < .1f) {
+				thisElevatorState = ElevatorState.PausingAtTop;
+			}
 			break;
 		case ElevatorState.Descending:
-
+			transform.Translate (Vector3.Normalize (StartPoint.position - EndPoint.position) * travelSpeed);
+			if (Vector3.Distance (transform.position, StartPoint.position) < .1f) {
+				thisElevatorState = ElevatorState.PausingAtBottom;
+			}
 			break;
 		case ElevatorState.PausingAtTop:
-
+			if (GenericTimer.RunGenericTimer (pauseTime, ref pauseTimer)) {
+				thisElevatorState = ElevatorState.Descending;
+			}
 			break;
 		case ElevatorState.PausingAtBottom:
-
+			if (GenericTimer.RunGenericTimer (pauseTime, ref pauseTimer)) {
+				thisElevatorState = ElevatorState.Ascending;
+			}
 			break;
 
 		}
