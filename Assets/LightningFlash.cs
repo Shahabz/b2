5e9b453229fcb89d2d6718094e9203616c44a784
paleonluna <paleonluna@gmail.com>
2017-06-06 00:00:01 -0700

    did visual audio and trigger pass on pier

---
+++ Assets/LightningFlash.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningFlash : MonoBehaviour {
+
+	[SerializeField]
+	Light thisLight;
+
+	// Use this for initialization
+	void OnEnable () {
+		StartCoroutine ("FlashLightning");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator FlashLightning () {
+		thisLight.enabled = true;
+		yield return new WaitForSeconds (.1f);
+		thisLight.enabled = false;
+
+		yield return new WaitForSeconds (.1f);
+		thisLight.enabled = true;
+
+		yield return new WaitForSeconds (.1f);
+		thisLight.enabled = false;
+
+	}
+}
---
+++ Assets/Scripts/LevelEvents/GenericTriggerEvent.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericTriggerEvent : MonoBehaviour {
+	public AudioSource[] OnEventTriggerSounds;
+	public AudioSource[] OnEventStopSounds;
+	public GameObject[]  DisableThisObject;
+	public GameObject[] EnableThisObject;
+	public ParticleSystem PlayThisParticle;
+	public ParticleSystem[] PlayTheseParticles;
+	bool hasPlayed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player" && !hasPlayed) {
+			hasPlayed = true;
+			foreach (AudioSource x in OnEventTriggerSounds) {
+				x.Play ();
+			}
+			foreach (AudioSource x in OnEventStopSounds) {
+				x.Stop ();
+			}
+			foreach (GameObject x in DisableThisObject) {
+				x.SetActive (false);
+			}
+			foreach (GameObject x in EnableThisObject) {
+				x.SetActive (true);
+			}
+			foreach (ParticleSystem x in PlayTheseParticles) {
+				x.gameObject.SetActive (true);
+				x.Play ();
+			}
+			if (PlayThisParticle!=null)
+				PlayThisParticle.Play ();
+		}
+	}
+}
