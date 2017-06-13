26ef1930a5f59d2b45dfdd5687ed43b6f42e65f0
paleonluna <paleonluna@gmail.com>
2017-06-12 22:12:24 -0700

    big update to factory level design

---
+++ Assets/Scripts/CopLights.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopLights : MonoBehaviour {
+
+	public GameObject[] redLights, blueLights;
+	float flashTime = 1f, flashTimer;
+	// Use this for initialization
+	void Start () {
+		foreach (GameObject x in redLights) {
+			x.SetActive (true);
+		}
+		foreach (GameObject x in blueLights) {
+			x.SetActive (false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GenericTimer.RunGenericTimer (flashTime, ref flashTimer)) {
+			AlternateLights ();
+		}
+	}
+
+	void AlternateLights() {
+		foreach (GameObject x in redLights) {
+			x.SetActive (!x.activeSelf);
+		}
+		foreach (GameObject x in blueLights) {
+			x.SetActive (!x.activeSelf);
+		}
+	}
+}
