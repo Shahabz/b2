247a8c529d725eff3d5520f282fc9134dc09d5f5
zachary rock-zelazny <zacharyrock@hotmail.com>
2017-08-06 03:29:33 -0700

    Fog wall in per. Collision on walls fixed for pier.

---
+++ Assets/Scripts/World/GlobalFogWall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalFogWall : MonoBehaviour {
+
+    public Transform fogWall;
+    public float minFog = 1f;
+    public float maxFog = 10f;
+
+    public float minDistance = 10f;
+
+    Transform player;
+
+	void Start () {
+        player = TestPlayerController.s_instance.transform;
+	}
+	
+	void Update () {
+        if (Vector3.Distance(fogWall.position, player.position) < minDistance)
+        {
+            GetComponent<UnityStandardAssets.ImageEffects.GlobalFog>().height = Mathf.Lerp(minFog, maxFog, Vector3.Distance(fogWall.position, player.position) / minDistance);
+        }
+//        else
+//        {
+//            GetComponent<UnityStandardAssets.ImageEffects.GlobalFog>().height = minDistance;
+//        }
+	}
+}
