0d1a46d66773e4fb368f5de37d5448b1ff8ac14d
paleonluna <paleonluna@gmail.com>
2017-08-15 20:25:47 -0700

    added in complete version of therapist and his place looks good

---
+++ Assets/Scripts/InteractableBook.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableBook : MonoBehaviour, IInteractable {
+
+	public GameObject[] pages;
+	int pageIterator;
+	bool isTurningPage;
+	bool turningPagesForward = true;
+	float lerpTimer, lerpTime = 2f;
+	float rotatePageAmount = 160f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isTurningPage) {
+			HandlePageTurn();
+		}
+	}
+
+	public void Interact() {
+		if (!isTurningPage) {
+			lerpTimer = 0;
+			isTurningPage = true;
+		}
+	}
+
+	void HandlePageTurn() {
+		lerpTimer += Time.deltaTime;
+
+		if (lerpTimer / lerpTime >= 1) {
+			OnPageTurnComplete ();
+			lerpTimer = lerpTime;
+			return;
+		}
+		if (turningPagesForward) {
+			float angle = Mathf.Lerp (0, rotatePageAmount, lerpTimer / lerpTime);
+			pages [pageIterator].transform.localRotation = Quaternion.Euler (angle, 0, 0);// (transform.position, transform.right, angle);
+		} else {
+			float angle = Mathf.Lerp (rotatePageAmount, 0, lerpTimer / lerpTime);
+			pages [pageIterator].transform.localRotation = Quaternion.Euler (angle, 0, 0);
+		}
+	}
+
+	void OnPageTurnComplete () {
+		isTurningPage = false;
+		if (turningPagesForward) {
+			if (pageIterator == pages.Length - 1) {
+				turningPagesForward = false;
+			} else {
+				pageIterator++;
+			}
+		} else {
+			if (pageIterator == 0) {
+				turningPagesForward = true;
+			} else {
+				pageIterator--;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/TimedEventTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedEventTrigger : UnityEventTrigger {
+
+	public float timeUntilTriggered;
+	float timer;
+	bool isBeingTimed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	protected override void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Player" && !hasPlayed) 
+		{
+			isBeingTimed = true;
+		}
+	}
+
+	void OnTriggerExit (Collider other) {
+		if (other.tag == "Player" && !hasPlayed) {
+			isBeingTimed = false;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (isBeingTimed)
+			timer += Time.deltaTime;
+		if (timer > timeUntilTriggered && !hasPlayed)
+			ExecuteEvent ();
+	}
+}
