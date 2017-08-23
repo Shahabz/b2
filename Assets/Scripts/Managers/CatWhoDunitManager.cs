8f794fd3aa1847efac8128cff9b02b551748d116
paleonluna <paleonluna@gmail.com>
2017-08-22 21:35:34 -0700

    added in WhoDunit puzzle script

---
+++ Assets/Scripts/Managers/CatWhoDunitManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatWhoDunitManager : MonoBehaviour {
+
+
+	int catsCompleted; //must complete five before win state can be activated, prevents random luck from succeeding
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
