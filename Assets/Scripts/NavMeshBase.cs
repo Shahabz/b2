162927b64eeed3122305b681aa9c2681d4722bcb
Pablo Leon-Luna <pabloleon-luna@Pablos-MacBook-Pro.local>
2017-09-20 23:38:15 -0700

    Added in teleport

---
+++ Assets/Scripts/NavMeshBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshBase : MonoBehaviour {
+
+
+	public NavMeshAgent thisNavMeshAgent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		thisNavMeshAgent.SetDestination (TestPlayerController.s_instance.transform.position);
+
+	}
+}
---
+++ Assets/Teleport.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+	public Transform teleportEnd;
+	Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Player") {
+			offset = TestPlayerController.s_instance.transform.position - transform.position;
+			TestPlayerController.s_instance.transform.position = teleportEnd.transform.position + offset;
+		}
+	}
+}
