1462bbd5e26d6613ed12c82672391dc9fed47103
Pablo <paleonluna@gmail.com>
2017-11-11 16:12:10 -0800

    think

--- Assets/Scripts/VictoriaSecretModel.cs
+++ Assets/Scripts/VictoriaSecretModel.cs
@@ -11,13 +11,19 @@ public class VictoriaSecretModel : MonoBehaviour, IInteractable {
 	public Vector3 navMeshTarget;
 	public Transform target;
 
-	bool bSwitchToWalking, bSwitchToStanding;
+	Vector3 cliffordPosition;
+
+	bool bSwitchToWalking, bSwitchToStanding, bSwitchToFiring;
 	public bool dontLookAtOnInteract;
 
 	void Start () {
 		thisNavMeshAgent = GetComponent<NavMeshAgent> ();
-		if (target)
+		if (target) {
+			print ("HIT");
 			navMeshTarget = target.position;
+			bSwitchToWalking = true;
+			SwitchToWalking ();
+		}
 	}
 	
 	// Update is called once per frame
@@ -28,12 +34,26 @@ public class VictoriaSecretModel : MonoBehaviour, IInteractable {
 		if (bSwitchToStanding) {
 			bSwitchToStanding = false;
 		}
+		if (bSwitchToFiring) {
+			SwitchToFiring ();
+			bSwitchToFiring = false;
+		}
+		if (Vector3.Distance (transform.position, target.position) < 3f) {
+			bSwitchToFiring = true;
+		}
 	}
 
 	public void Interact() {
 		if (!dontLookAtOnInteract)
 	 		transform.LookAt (TestPlayerController.s_instance.transform);
 		GetComponent<DialogueSystem> ().StartDialogue ();
+	}
+
+	public void SwitchToFiring() {
+		thisNavMeshAgent.isStopped = true;
+		GetComponent<Animator> ().SetTrigger ("fire");
+		cliffordPosition = GameObject.FindGameObjectWithTag ("CLIFFORD").transform.position;
+		transform.LookAt (cliffordPosition);
 	}
 
 	public void SwitchToStanding() {
@@ -43,8 +63,7 @@ public class VictoriaSecretModel : MonoBehaviour, IInteractable {
 	}
 
 	public void SwitchToWalking() {
-		thisNavMeshAgent.isStopped = false;
-		thisNavMeshAgent.SetDestination (navMeshTarget);
-		GetComponent<Animator> ().SetTrigger ("walk");
+		thisNavMeshAgent.SetDestination (target.position);
+		GetComponent<Animator> ().SetTrigger ("run");
 	}
 }
