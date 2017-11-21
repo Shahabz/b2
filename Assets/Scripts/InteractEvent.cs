b7a76f879798a4f7602ad2bc4cf37d32876c7134
Pablo <paleonluna@gmail.com>
2017-11-21 00:32:11 -0800

    fixed all censorship, added teddy bear, added note

---
+++ Assets/Scripts/InteractEvent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InteractEvent : MonoBehaviour, IInteractable {
+
+	public UnityEvent OnInteractEvents;
+
+	public void Interact() {
+		OnInteractEvents.Invoke ();
+	}
+}
