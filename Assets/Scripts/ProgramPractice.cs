2f0d99c7f802faf4446de141ac2e469c9d45ab1f
Pablo <paleonluna@gmail.com>
2017-11-29 23:23:49 -0800

    added a ton of content

---
+++ Assets/Scripts/ProgramPractice.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgramPractice : MonoBehaviour {
+	bool isProgramming;
+	float timer, time = 5f;
+	int counter;
+	public Transform spawnhere;
+
+	public GameObject Explosion;
+	public GameObject[] DestroyThese;
+	public Cinemachine.CinemachineVirtualCamera thisCamera;
+	// Use this for initialization
+	public void StartProgramming () {
+
+		if (!isProgramming) {
+			if (counter < 2) {
+				isProgramming = true;
+				GetComponent<CodeThoughts> ().StartSpawning (false);
+				thisCamera.enabled = true;
+				counter++;
+			} else {
+				isProgramming = true;
+				GetComponent<CodeThoughts> ().StartSpawning (true);
+				thisCamera.enabled = true;
+				counter++;
+
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isProgramming)
+			timer += Time.deltaTime;
+		if (timer > time)
+			StopProgramming ();
+	}
+
+	void StopProgramming ()
+	{
+		GetComponent<CodeThoughts> ().StopSpawning ();
+		isProgramming = false;
+		thisCamera.enabled = false;
+		timer = 0;
+		if (counter > 2)
+			DestroyComputer ();
+	}
+
+	void DestroyComputer ()
+	{
+		Instantiate (Explosion,spawnhere);
+
+		TestPlayerController.s_instance.GetComponent<HealthHandler> ().TakeStress (10);
+		TestPlayerController.s_instance.GetComponent<Animator> ().SetTrigger ("punch");
+		foreach (GameObject x in DestroyThese) {
+			Destroy (x);
+		}
+	}
+}
